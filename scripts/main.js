5cb22dc0863fe7e675e1345fc48710b2c09af105
KaspaHub <ivoclassic@pm.me>
2025-06-01 11:39:50 +0300

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+function injectContent() {
+    document.getElementById('page-footer').innerHTML = '2025 Kaspa Community Hub';
+    document.getElementById('page-nav').innerHTML = '<h2><a href="/">Kaspa Hub</a></h2>';
+}
+injectContent();
